91e42b1a02ed2ced7a527fd0210cc873ca9d13b1
Alice Zoë Bevan–McGregor <amcgregor@cegid.com>
2022-06-16 11:16:56 -0400

    Armor against httpx/requests differences.

--- test/process/test_validation.py
+++ test/process/test_validation.py
@@ -1,11 +1,15 @@
 # It is often desirable to proactively catch errors being returned by third-party APIs.
 # You can mix-in the Validated class to apply this behavior.
 
-from httpx import HTTPStatusError
 from pytest import fixture, raises
 
 from web.api.client import Interface
 from web.api.processing import Validated
+
+try:
+	from httpx import HTTPStatusError as HTTPError
+except ImportError:
+	from requests import HTTPError
 
 
 class TInterface(Validated, Interface): pass
@@ -27,10 +31,10 @@ class TestValidation:
 	#	__import__('wdb').set_trace()
 	
 	def test_client_error(self, iface):
-		with raises(HTTPStatusError):
+		with raises(HTTPError):
 			iface.status[400].get()
 	
 	def test_server_error(self, iface):
-		with raises(HTTPStatusError):
+		with raises(HTTPError):
 			iface.status[500].get()
 
