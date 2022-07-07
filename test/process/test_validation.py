352880f9c8c9c5d9c61c81e7f2f4199bbe3cbda5
Alice Zoë Bevan–McGregor <amcgregor@cegid.com>
2022-07-07 16:24:36 -0400

    Serialization tests.
    JSON and XML are unconditional; built-in Python functionality. HTML is dependent on the presence of the `pyquery` package.

--- test/process/test_validation.py
+++ test/process/test_validation.py
@@ -1,10 +1,10 @@
 # It is often desirable to proactively catch errors being returned by third-party APIs.
 # You can mix-in the Validated class to apply this behavior.
 
-from pytest import fixture, raises
+from pytest import fixture, importorskip, raises
 
 from web.api.client import Interface
-from web.api.processing import Body, Text, Validated
+from web.api.processing import Body, Serialized, Text, Validated
 
 try:
 	from httpx import HTTPStatusError as HTTPError
@@ -61,3 +61,34 @@ class TestBodyRetrieval:
 	def test_xml_text(self, iface_t):
 		result = iface_t.xml.get()
 		assert result.startswith('<?xml')
+
+
+@fixture
+def iface_s():
+	class SInterface(Serialized, Interface): pass
+	
+	interface = SInterface('https://httpbin.org/')
+	yield interface
+	interface._ua.close()
+
+
+class TestSerializedRetrieval:
+	"""Serialized content retrieval attempts to decode returned content as appropriate."""
+	
+	def test_html(self, iface_s):
+		importorskip('pyquery')
+		
+		result = iface_s.html.get()
+		
+		assert result('h1').length == 1
+	
+	def test_json(self, iface_s):
+		result = iface_s.json.get()
+		
+		assert 'slideshow' in result
+		assert not {'author', 'date', 'slides', 'title'} ^ set(result['slideshow'])
+	
+	def test_xml(self, iface_s):
+		result = iface_s.xml.get()
+		
+		assert not {'title', 'date', 'author'} ^ set(result.keys())
