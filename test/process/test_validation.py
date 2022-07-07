8c639aeb765361d8950a82ca0bc246ccaf7e7cf7
Alice Zoë Bevan–McGregor <amcgregor@cegid.com>
2022-07-07 15:45:24 -0400

    Body and Text processing tests.

--- test/process/test_validation.py
+++ test/process/test_validation.py
@@ -4,7 +4,7 @@
 from pytest import fixture, raises
 
 from web.api.client import Interface
-from web.api.processing import Validated
+from web.api.processing import Body, Text, Validated
 
 try:
 	from httpx import HTTPStatusError as HTTPError
@@ -12,24 +12,52 @@ except ImportError:
 	from requests import HTTPError
 
 
-class TInterface(Validated, Interface): pass
-
-
 @fixture
-def iface():
-	interface = TInterface('https://httpbin.org/')
+def iface_v():
+	class VInterface(Validated, Interface): pass
+	
+	interface = VInterface('https://httpbin.org/')
 	yield interface
 	interface._ua.close()
 
 
 class TestValidation:
-	def test_success(self, iface):
-		iface.status[200].get()
+	def test_success(self, iface_v):
+		iface_v.status[200].get()
 	
-	def test_client_error(self, iface):
+	def test_client_error(self, iface_v):
 		with raises(HTTPError):
-			iface.status[400].get()
+			iface_v.status[400].get()
 	
-	def test_server_error(self, iface):
+	def test_server_error(self, iface_v):
 		with raises(HTTPError):
-			iface.status[500].get()
+			iface_v.status[500].get()
+
+
+@fixture
+def iface_b():
+	class BInterface(Body, Interface): pass
+	
+	interface = BInterface('https://httpbin.org/')
+	yield interface
+	interface._ua.close()
+
+@fixture
+def iface_t():
+	class TInterface(Text, Interface): pass
+	
+	interface = TInterface('https://httpbin.org/')
+	yield interface
+	interface._ua.close()
+
+
+class TestBodyRetrieval:
+	"""Body retrieval returns response.content, which is binary."""
+	
+	def test_xml_binary(self, iface_b):
+		result = iface_b.xml.get()
+		assert result.startswith(b'<?xml')
+	
+	def test_xml_text(self, iface_t):
+		result = iface_t.xml.get()
+		assert result.startswith('<?xml')
